3cd802097f380eb1cac6caa4b9797cf12ecea0f0
cmvargasdev <110634337+cmvargasdev@users.noreply.github.com>
2022-09-23 14:40:29 -0500

    Clase Alumno

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -1,1 +1,19 @@
+class Alumno():
+    def __init__(self, nombre, nota):
+        self.nombre   = nombre
+        self.nota = nota
 
+    def imprimir(self):
+        print("Nombre: ",self.nombre)
+        print("Nota: ",self.nota)
+
+    def isAprobado(self):
+            if self.nota < 10:
+                print("Alumno No Aprobado")
+            else:
+                print("Alumno Aprobado")
+
+
+alumno = Alumno("Miguel", 9)
+alumno.imprimir()
+alumno.isAprobado()
